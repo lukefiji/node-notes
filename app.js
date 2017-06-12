2ec60e23ea093137746ded648e11c6b492070cdd
Luke Fiji <luke.fiji@gmail.com>
2017-06-12 00:17:54 -0700

    Initial commit - first video done

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("Starting app");
+
+const fs = require("fs");
+const os = require("os");
+
+var user = os.userInfo();
+
+fs.appendFile("greetings.txt", `Hello ${user.username}!`, err => {
+  if (err) throw err;
+});
