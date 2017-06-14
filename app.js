63c8c4e0fdefa2d43bbb00eb6f8a4ccd9f778d1f
Luke Fiji <luke.fiji@gmail.com>
2017-06-13 17:21:21 -0700

    Need internet connection to continue

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ console.log("Starting app");
 
 const fs = require("fs");
 const os = require("os");
+const notes = require("./notes");
 
-var user = os.userInfo();
+// var user = os.userInfo();
 
-fs.appendFile("greetings.txt", `Hello ${user.username}!`, err => {
-  if (err) throw err;
-});
+// fs.appendFile("greetings.txt", `Hello ${user.username}!`, err => {
+//   if (err) throw err;
+// });
